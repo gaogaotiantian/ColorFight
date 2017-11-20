881c10d2513d2238c36d65d7950d3297dba19e33
高天 <gaogaotiantian@hotmail.com>
2017-11-19 17:34:17 -0800

    Change it back

--- app.py
+++ app.py
@@ -387,8 +387,8 @@ def StartGame():
         aiOnly = True
     else:
         aiOnly = False
-    width =  15
-    height = 15
+    width =  30
+    height = 30
     currTime = GetCurrDbTimeSecs()
     if data['last_time'] != 0:
         endTime = currTime + data['last_time']
