b51552bb8928338cb4a01c6d711011735e8d072a
Tian Gao <gaotian@Tians-MacBook-Pro-10.local>
2019-01-03 02:28:53 -0800

    Add first changes

--- static/admin.js
+++ static/admin.js
@@ -1,5 +1,5 @@
-//hostUrl = "http://localhost:8000/"
-hostUrl = "https://colorfight.herokuapp.com/"
+//hostUrl = "http://localhost:5000/"
+hostUrl = "https://colorfight-dev.herokuapp.com/"
 CreateGame = function(soft) {
     var aiOnly = false;
     if ($('#ai_only').find(":selected").text() == 'Yes') {
